0581204bf1eabf09b5b3607adb2f6a0bfe22f23a
unknown <aaron.soussan@ortmontreuil.fr>
2024-11-27 14:50:40 +0100

    Add GitHub Actions workflow for CI/CD

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yehi Ameleh
+          Aaron Soussan
         </p>
         <a
           className="App-link"
